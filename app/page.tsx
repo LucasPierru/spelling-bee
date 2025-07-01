70f862f2f28dadef40863ddfedc16499b35ef261
Lucas Pierru <lucaspierru7@gmail.com>
2025-07-01 00:08:31 -0400

    added pointer on theme toggle and short description of the game

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,11 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col items-center max-w-5xl h-[calc(100vh-5rem)] mx-auto p-4 md:py-56">
-      <h1 className="text-4xl font-bold mb-6">Word Hive</h1>
+      <h1 className="text-4xl font-bold mb-4">Word Hive</h1>
+      <h2 className="text-xl font-semibold mb-1">Find as many words as you can!</h2>
+      <span className="text-lg mb-2">
+        Each words must must include the central letter and must be 4 letters minimum
+      </span>
       <WordForm
         letters={scrambledLetters}
         centerLetter={centerLetter}
