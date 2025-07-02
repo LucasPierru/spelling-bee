b15670b4da7a60248a49b6d72e5d3485927ce5b0
Lucas Pierru <lucaspierru7@gmail.com>
2025-07-02 18:37:25 -0400

    changed upsert to check and create

--- app/api/generate-letters/route.ts
+++ app/api/generate-letters/route.ts
@@ -10,12 +10,15 @@ export async function GET() {
 
     const letters: string[] = generateLetterSet();
     const centerLetter = letters[0]
-    const result = await DailyLetters.findOneAndUpdate(
-      { date },
-      { letters, centerLetter },
-      { new: true, upsert: true }
-    );
-    return Response.json({ success: true, data: result });
+    const existingLetters = await DailyLetters.findOne({ date })
+    if (existingLetters) {
+      return Response.json({ success: true, data: existingLetters });
+    } else {
+      const result = await DailyLetters.create(
+        { date, letters, centerLetter },
+      );
+      return Response.json({ success: true, data: result });
+    }
   } catch (error) {
     console.error('Failed to add daily letters:', error);
     return Response.json({ success: false, error: 'Failed to add letters of the day.' });
